ff3da61c375d03ae6458cff79e748e99ce6476bb
mahan <mn.nakhei98@gmail.com>
2023-03-16 06:50:47 +0330

    use List array and add method to print the Relation between our professor and our student

--- UniversityApplication.java
+++ UniversityApplication.java
@@ -1,9 +1,15 @@
 public class UniversityApplication {
     public static void main(String[] args) {
-        Professor professor1 = new Professor("ali","nassery","math");
-        Professor professor2 = new Professor("amir","jafary","physics");
-        Student student1 = new Student("arman","zanbili",6,"Computer",19);
-        Student student2 = new Student("mohammad","sedghi",7,"computer science",20);
-
+        Professor professor1 = new Professor("ali", "nassery", "math");
+        Professor professor2 = new Professor("amir", "jafary", "physics");
+        Student student1 = new Student("arman", "zanbili", 6, "Computer", 19);
+        Student student2 = new Student("mohammad", "sedghi", 7, "computer science", 20);
+        professor1.addStudent(student1);
+        professor1.addStudent(student2);
+        professor2.addStudent(student2);
+        professor1.print();
+        professor2.print();
+        student1.print();
+        student2.print();
     }
 }
