8fd76699f7a37cf16dc509972285193083319382
mahan <mn.nakhei98@gmail.com>
2023-03-16 06:31:44 +0330

    added some feature like course to our Professor class and include it in our constructor and give it a get method and added to our print method

--- Professor.java
+++ Professor.java
@@ -1,21 +1,27 @@
 import java.util.ArrayList;
 import  java.util.List;
 public class Professor {
-    private String name;
-    private String lastname;
-    private final List<Student> students = new ArrayList();
+    private final String name;
+    private final String lastname;
+    private final String course;
+    private final List<Student> students = new ArrayList<>();
 
     public String getName() {
         return name;
+    }
+
+    public String getCourse() {
+        return course;
     }
 
     public String getLastname() {
         return lastname;
     }
 
-    public Professor(String firstname, String lastname) {
+    public Professor(String firstname, String lastname, String course) {
         this.name = firstname;
         this.lastname = lastname;
+        this.course = course ;
     }
     public void addStudent(Student student) {
         student.addProfessor(this);
@@ -26,9 +32,10 @@ public class Professor {
         return students;
     }
     public void print() {
-        System.out.println("Professor " + this.name + "'s students are:");
+        System.out.println("Professor " + this.name + this.lastname + this.course + "'s students are:");
         for (Student student:this.students) {
-            System.out.println("- " + student.getName());
+            System.out.println("- " + student.getName() +student.getLastName() + student.getTerm() + student.getTerm()
+                    + student.getFieldOfStudy() + student.getScore());
         }
     }
 
