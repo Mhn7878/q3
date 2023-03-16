d4c8d91b121e209f94c5c5ad9be2f881e934b472
mahan <mn.nakhei98@gmail.com>
2023-03-16 05:22:16 +0330

    added a constructor and a getname and get last name for our Professor class

--- Professor.java
+++ Professor.java
@@ -4,4 +4,19 @@ public class Professor {
     private String firstname;
     private String lastname;
     private final List<Student> students = new ArrayList();
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public Professor(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+
 }
