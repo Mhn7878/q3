1d37bc0a1db1654c8bc99762fed90390f04ffaf2
mahan <mn.nakhei98@gmail.com>
2023-03-16 06:20:21 +0330

    added some feature like term , score and field of study to our Student class and added to our constructor and created a get method for them andinclude them in our print method

--- Student.java
+++ Student.java
@@ -1,9 +1,26 @@
 import java.util.ArrayList;
-import  java.util.List;
+import java.util.List;
+
 public class Student {
     private String name;
     private String lastName;
+    private int term;
+    private String fieldOfStudy;
+    private double score;
+
+    public int getTerm() {
+        return term;
+    }
+
     private final List<Professor> professors = new ArrayList<>();
+
+    public double getScore() {
+        return score;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
 
     public String getName() {
         return name;
@@ -12,6 +29,7 @@ public class Student {
     public String getLastName() {
         return lastName;
     }
+
     public void addProfessor(Professor professor) {
         this.professors.add(professor);
     }
@@ -20,13 +38,17 @@ public class Student {
         return professors;
     }
 
-    public Student(String name, String lastName) {
+    public Student(String name, String lastName, int term , String fieldOfStudy , double score) {
         this.name = name;
         this.lastName = lastName;
+        this.term = term;
+        this.fieldOfStudy = fieldOfStudy;
+        this.score = score;
     }
+
     public void print() {
-        System.out.println("Student " + this.name + "'s teachers are:");
-        for (Professor teacher:this.professors) {
+        System.out.println("Student " + this.name + this.lastName + this.term + this.fieldOfStudy + this.score + "'s professors are:");
+        for (Professor teacher : this.professors) {
             System.out.println("- " + teacher.getName());
         }
     }
