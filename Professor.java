511d64ca5ea5dfd8d7015708138c5a6d04147c1c
mahan <mn.nakhei98@gmail.com>
2023-03-16 05:19:18 +0330

    added a name and last name and crated a array list for our Professor class

--- Professor.java
+++ Professor.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import  java.util.List;
 public class Professor {
-    
+    private String firstname;
+    private String lastname;
+    private final List<Student> students = new ArrayList();
 }
