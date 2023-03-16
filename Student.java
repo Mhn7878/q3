db496e1b62fc92e5c823f7a13fa9f977837caf71
mahan <mn.nakhei98@gmail.com>
2023-03-16 05:35:26 +0330

    added some feature to our Student class

--- Student.java
+++ Student.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import  java.util.List;
 public class Student {
-    
+    private String name;
+    private String lastName;
+    private final List<Professor> professors = new ArrayList<>();
 }
